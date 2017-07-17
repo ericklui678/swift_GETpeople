5cc595b4ce495d1fc93eb5b586ade79a3a4d5679
Erick Lui <erickjin.lui@gmail.com>
2017-07-17 12:12:37 -0700

    added films tab to retrieve titles

--- GETpeople/FilmTableTableViewController.swift
+++ GETpeople/FilmTableTableViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FilmTableTableViewController.swift
 //  GETpeople
 //
 //  Created by Erick Lui on 7/17/17.
@@ -8,29 +8,22 @@
 
 import UIKit
 
-class PeopleViewController: UITableViewController {
-  // people as an array of dictionaries
-  var people = [String]()
+class FilmTableTableViewController: UITableViewController {
   
+  var films = [String]()
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    // specify URL
-    let url = URL(string: "http://swapi.co/api/people")
-    // create URLSession to handle the request tasks
+    let url = URL(string: "http://swapi.co/api/films")
     let session = URLSession.shared
-    // create task to make the request and run the completion handler
     let task = session.dataTask(with: url!, completionHandler: {
       data, response, error in
       do {
-        // Try converting data object into JSON and typecast as dictionary type
         if let jsonResult = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary {
-          // If there is results array, cast as Array
           if let results = jsonResult["results"] as? NSArray {
-            // For each dictionary in the array
-            for person in results {
-              // Cast each object as dictionary
-              let personDict = person as! NSDictionary
-              self.people.append(personDict["name"]! as! String)
+            for object in results {
+              let film = object as! NSDictionary
+              self.films.append(film["title"]! as! String)
               DispatchQueue.main.async {
                 self.tableView.reloadData()
               }
@@ -38,7 +31,7 @@ class PeopleViewController: UITableViewController {
           }
         }
       } catch {
-        print(error)
+        print("Error", error)
       }
     })
     task.resume()
@@ -46,16 +39,12 @@ class PeopleViewController: UITableViewController {
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
   }
-  override func numberOfSections(in tableView: UITableView) -> Int {
-    return 1
-  }
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return people.count
+    return films.count
   }
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = UITableViewCell()
-    cell.textLabel?.text = people[indexPath.row]
+    cell.textLabel?.text = films[indexPath.row]
     return cell
   }
 }
-
